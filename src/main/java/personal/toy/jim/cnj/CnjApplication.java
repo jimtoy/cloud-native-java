dc4eb479fb1d2f9f05e26b5a304bc681f7104c3e
Jim Toy <jim.toy@gmail.com>
2018-10-08 22:01:15 -0400

    Initial commit

---
+++ src/main/java/personal/toy/jim/cnj/CnjApplication.java
@@ -0,0 +1,12 @@
+package personal.toy.jim.cnj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnjApplication.class, args);
+	}
+}
