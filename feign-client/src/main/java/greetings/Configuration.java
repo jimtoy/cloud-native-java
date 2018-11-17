01c1ab01042edaa12deb43c168073b8d17b74f1f
Jim Toy <jim.toy@gmail.com>
2018-11-16 20:26:40 -0500

    get everything to green

---
+++ feign-client/src/main/java/greetings/Configuration.java
@@ -0,0 +1,16 @@
+package greetings;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
