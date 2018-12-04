2e4f9b73869ab91c0b6398f08ba573c1886d009d
Jim Toy <jim.toy@gmail.com>
2018-12-03 20:12:19 -0500

    Preparing for spring security and setting up OAuth

---
+++ secure-client/src/main/java/com/example/secureclient/SecureClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.secureclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureClientApplication.class, args);
+    }
+}
