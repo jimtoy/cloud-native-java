c94dce76576ea63bac498053876cdb4cbeae1e88
Jim Toy <jim.toy@gmail.com>
2018-12-05 19:15:38 -0500

    Moving on to the auth-server

---
+++ auth-service/src/main/java/auth/clients/ClientConfiguration.java
@@ -0,0 +1,12 @@
+package auth.clients;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfiguration {
+
+    //private final LoadBalancerClient loadBalancerClient;
+}
+
+
